7b54464fdcb38f3ae977287b4603af06a034a1fd
edson <edsonaristiguieta@gmail.com>
2024-04-21 18:21:10 +0200

    First commit

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,5 @@
+export class Product {
+  public id: number; // 1,2,3,4,5,6,7
+  public name: string;
+  public price: number;
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,82 @@
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsService');
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Database connected');
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto,
+    });
+  }
+
+  async findAll(paginationDto) {
+    const { page, limit } = paginationDto;
+
+    const totalPages = await this.product.count({ where: { available: true } });
+
+    const lastPage = Math.ceil(totalPages / limit);
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { available: true },
+      }),
+      meta: {
+        total: totalPages,
+        page: page, // se puede quitar por ecma6
+        lastPage: lastPage,
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id, available: true },
+    });
+
+    if (!product) {
+      throw new NotFoundException(`producto con el id ${id} no encontrado `);
+    }
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const { id: _, ...data } = updateProductDto;
+
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: data,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    //logica
+
+    const product = await this.product.update({
+      where: { id },
+      data: { available: false },
+    });
+
+    /*    return this.product.delete({
+      where: { id },
+    }); */ //eliminacion total
+    return product;
+  }
+}
